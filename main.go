e9133bebe24abb27a7a366417ac44e017f65e89f
imankit007 <ankit4568007@gmail.com>
2025-08-03 14:24:50 +0530

    Add Swagger docs

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"sample.com/crud-api/config"
 	"sample.com/crud-api/initializers"
 	"sample.com/crud-api/router"
@@ -20,11 +23,23 @@ func main() {
 	init := config.Init()
 	app := router.Init(init)
 
+	// url := ginSwagger.URL("http://localhost:9000/swagger/doc.json")
+
+
 	app.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Ankit",
 		})
 	})
 
-	app.Run("0.0.0.0:8080")
+
+	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+
+	err := app.Run("0.0.0.0:8080")
+
+	if err != nil{
+		log.Fatal(err)
+	}
+
 }
